5239876c68777ef79150b7caf68dd5999f6530a6
alzemand <edilson_alz@hotmail.com>
2018-09-26 13:55:46 -0300

    ajuste de representação

--- models/OO_validators.py
+++ models/OO_validators.py
@@ -180,6 +180,11 @@ class IS_CNPJ(object):
 def to_cnpj(value):
     return "%s.%s.%s/%s-%s" % ( value[0:2], value[2:5], value[5:8], value[8:12], value[12:14] )
 
+def to_cpf(value):
+    return "%s.%s.%s-%s" % ( value[0:3], value[3:6], value[6:9], value[9:11])
+
+def to_rg(value):
+    return "%s.%s.%s-%s" % ( value[0:2], value[2:5], value[5:8], value[8:])
 
 class IS_CEP(object):
     def __init__(self, format=True, error_message='Digite apenas os números!'):
